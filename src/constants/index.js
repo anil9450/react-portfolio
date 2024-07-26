92399a3df1afc43fb0f99402c6d174df28943610
ANIL KUMAR SINGH KUSHWAHA <90541938+anil9450@users.noreply.github.com>
2024-07-26 22:46:12 +0530

    Personel-Portfolio

---
+++ src/constants/index.js
@@ -0,0 +1,90 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `With 2 years of hands-on experience in frontend development, I
+                have honed my skills in creating dynamic, user-friendly web
+                applications. My expertise spans a variety of projects, from
+                management systems to personal websites, showcasing my ability
+                to adapt and innovate in different technological environments.`;
+
+export const ABOUT_TEXT = `I am a frontend developer with two years of professional experience, specializing in crafting engaging and responsive web applications. My expertise spans across various technologies, including JavaScript, HTML, CSS, React, Tailwind, Next.js, and Bootstrap. I have successfully contributed to and led several projects, such as the Battery Management System, Trip Management System, Route Management System, Live Track Page, Control Panel, and E-school. These projects often involve integration with IoT devices, offering features like live tracking, notifications, and comprehensive data reporting.
+
+I am also the creator of websites like Delta Executor and Single Line Script, which showcase my skills in React and Tailwind. My portfolio includes developing dynamic interfaces, user authentication systems, and interactive elements such as Login-Sign up Pages, Blog Websites, To-do Lists, and Background Changers. I am passionate about building intuitive and efficient digital experiences, continuously learning and staying updated with the latest trends and technologies in web development.`;
+export const EXPERIENCES = [
+  {
+    year: "April - 2022 -April - 2024",
+    role: "Frontend Developer",
+    company: "Geocrat Technologies Private Limited",
+    description: `Worked closely with backend developers, designers, and project managers to deliver cohesive and efficient web applications.
+    Developed and integrated new features into existing applications, including complex user interfaces, dynamic data displays, and interactive elements.
+    Identified and resolved issues in the frontend code, ensuring smooth and bug-free operation of the applications.`,
+    technologies: ["Javascript", "Bootstrap", "CSS", "HTML", "Django"],
+  },
+  {
+    year: "2024 May - Present",
+    role: "Frontend Developer",
+    company: "Monetiscope LLP",
+    description: `Designed and developed user interfaces using Next.js , Reactjs & TailwindCSS. Implemented responsive designs and optimized frontend performance.`,
+    technologies: [
+      "Javascript",
+      "React",
+      "Nextjs",
+      "TaiwindCSS",
+      "JSX",
+      "HTML",
+    ],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Battery Management System",
+    image: project1,
+    description:
+      "This system provided real-time monitoring and management of battery health and performance. It featured dashboards displaying battery status, charge levels, and usage patterns. The system also included notifications for maintenance and alerts for abnormal conditions.",
+    technologies: ["HTML", "CSS", "Javascript", "Bootstrap", "Django"],
+  },
+  {
+    title: "Trip Management System",
+    image: project2,
+    description:
+      "This application facilitated the management of trips for logistics and transportation companies. It allowed users to plan routes, track vehicle locations, and manage schedules. Features included route optimization, trip history, and detailed reporting.",
+    technologies: ["HTML", "CSS", "Javascript", "Bootstrap", "Django"],
+  },
+  {
+    title: "Control Panel",
+    image: project3,
+    description:
+      "A comprehensive dashboard for managing various IoT-connected devices and systems. The control panel allowed users to monitor device status, control settings, and receive alerts. It included features like data visualization, user management, and system diagnostics.",
+    technologies: ["HTML", "CSS", "Javascript", "Bootstrap", "Django"],
+  },
+  {
+    title: "E-school",
+    image: project4,
+    description:
+      "An educational platform designed to provide online learning resources and tools. The platform included course management, student tracking, and interactive content delivery. It supported features like video lectures, quizzes, and progress tracking.",
+    technologies: ["HTML", "CSS", "Javascript", "Bootstrap", "Django"],
+  },
+  {
+    title: "Delta-Executor",
+    image: project4,
+    description:
+      "The Delta Executor game project sounds intriguing! Could you provide more details about the project? Specifically, information such as the gameplay mechanics, target audience, platform (web, mobile, desktop), and any unique features or technologies used would be helpful. This will allow me to give you a more comprehensive overview or assistance.",
+    technologies: ["Javascript", "Reactjs", "Nextjs", "tailwindCSS", "JSX"],
+  },
+  {
+    title: "One Liner Script Multiple Script",
+    image: project4,
+    description:
+      "One Liner Script Multiple Script project seems to focus on creating a platform where users can access and utilize various scripts or other tasks,referring to specific features, functionalities, or a particular use case, please share more details! This will help in providing a more accurate and detailed description.",
+    technologies: ["Javascript", "Reactjs", "Nextjs", "tailwindCSS", "JSX"],
+  },
+];
+
+export const CONTACT = {
+  address: "Mahadevpura Udaynagar Bangaluru - 560048",
+  phoneNo: "+91 9450813596 ",
+  email: "kanil9128@gmail.com",
+};
