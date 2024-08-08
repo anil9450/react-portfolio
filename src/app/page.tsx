19d97ad822bb147410f95c5f4e6ad3d4b4b610c8
ANIL KUMAR SINGH KUSHWAHA <90541938+anil9450@users.noreply.github.com>
2024-08-08 12:08:45 +0530

    scroll issue fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         <div className="fixed top-0 -z-10 h-full w-full"></div>
       </div>
       <div
-        className="absolute top-0 z-[-2] h-screen w-screen bg-nuetral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,
+        className="top-0 z-[-2] w-screen bg-nuetral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,
       rgba(120,119,198,0.3),rgba(255,255,255,0))]"
       ></div>
 
